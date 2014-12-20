bbb459b87c65cfe0ef0eaa1aaed2b4f3c7d09065
Svetoslav Popov <svetoslavpopov93@gmail.com>
2014-12-20 03:57:06 +0200

    Fixed app.js file

---
+++ AngularJSSAPProject/AngularJSSAPProject/scripts/app.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="angular.js" />
+/// <reference path="controller.js" />
+
+(function () {
+    var mainModule = angular.module('mainModule', []);
+
+    mainModule.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/', {
+            templateUrl: 'views/home-view.html',
+            controller: 'PublicScreenView'
+        }).
+        when('/login', {
+            templateUrl: 'views/login-view.html',
+            controller: 'PublicScreenView'
+        }).
+        otherwise({
+            redirectTo: '/'
+        });
+  }]);
+
+
+    function PublicScreenView($scope) {
+        $scope.asd = 'asdadad';
+    }
+
+    mainModule.controller('PublicScreenView', PublicScreenView);
+}());
---
+++ AngularJSSAPProject/AngularJSSAPProject/scripts/controllers.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.js" />
+
+var Controllers = (function () {
+    function login() {
+        
+    }
+
+    function register() {
+
+    }
+
+    return {
+        login: login,
+        register: register
+    };
+}());
